ee312e502f932c87ce218623047d4b4861446e03
lauramoore <laura.moore@rightbox.com>
2023-02-21 06:40:42 -0500

    starter dependencies

---
+++ routes/networktables.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var wpilibclient = require('wpilib-nt-client')
+
+const client = new ntClient.Client()
+ 
+
+
+/* GET users listing. */
+
+router.get('/', function(req, res, next) {
+  // Connects the client to the server on team 3571's roborio
+client.start((isConnected, err) => {
+  // Displays the error and the state of connection
+   res.send({ isConnected, err });
+}, 'roborio-3571.local');
+
+
+});
+
+module.exports = router;
+
